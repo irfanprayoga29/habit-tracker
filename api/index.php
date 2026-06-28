82f0f0e81d8b9828747ea6077bbea205be20a29c
Apon <irfanprayoga29@gmail.com>
2026-06-29 01:27:41 +0700

    Fix Vercel 500 error by redirecting storage to /tmp

--- api/index.php
+++ api/index.php
@@ -5,6 +5,25 @@ error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
 ini_set('display_errors', '0');
 
 /**
+ * Create writable storage directories in /tmp for Vercel
+ */
+$tmpStorage = '/tmp/storage';
+if (!is_dir($tmpStorage)) {
+    $dirs = [
+        "$tmpStorage/app",
+        "$tmpStorage/framework/cache/data",
+        "$tmpStorage/framework/sessions",
+        "$tmpStorage/framework/views",
+        "$tmpStorage/logs",
+    ];
+    foreach ($dirs as $dir) {
+        if (!is_dir($dir)) {
+            mkdir($dir, 0777, true);
+        }
+    }
+}
+
+/**
  * Forward Vercel requests to normal index.php
  */
 require __DIR__ . '/../public/index.php';
