91d1fe50330c39c9337ef78558dbddf5149e28c6
Apon <irfanprayoga29@gmail.com>
2026-06-29 01:25:35 +0700

    Suppress PHP deprecation warnings in Vercel

--- api/index.php
+++ api/index.php
@@ -1,5 +1,9 @@
 <?php
 
+// Suppress deprecation warnings on Vercel PHP 8.4+
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
+ini_set('display_errors', '0');
+
 /**
  * Forward Vercel requests to normal index.php
  */
